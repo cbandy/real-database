42c76064a6e2516eadc2c0fbadfc434b02c55f44
Chris Bandy <chris.bandy@kohanaphp.com>
2010-01-07 09:38:43 -0600

    Query expression

---
+++ classes/database/query.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @package RealDatabase
+ *
+ * @author      Chris Bandy
+ * @copyright   (c) 2010 Chris Bandy
+ * @license     http://www.opensource.org/licenses/isc-license.txt
+ */
+class Database_Query extends Database_Expression
+{
+
+}
