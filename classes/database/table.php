68360c88864f48890ed62dddda2b853dd0497c06
Chris Bandy <chris.bandy@kohanaphp.com>
2010-01-15 00:40:49 -0600

    Database identifiers

---
+++ classes/database/table.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @package RealDatabase
+ *
+ * @author      Chris Bandy
+ * @copyright   (c) 2010 Chris Bandy
+ * @license     http://www.opensource.org/licenses/isc-license.txt
+ */
+class Database_Table extends Database_Identifier
+{
+
+}
