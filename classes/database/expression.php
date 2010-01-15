d4ee876d5c5058ed204c05111f9984fb798670e0
Chris Bandy <chris.bandy@kohanaphp.com>
2010-01-15 01:04:59 -0600

    Implement Countable to detect "empty" expressions

--- classes/database/expression.php
+++ classes/database/expression.php
@@ -7,7 +7,7 @@
  * @copyright   (c) 2010 Chris Bandy
  * @license     http://www.opensource.org/licenses/isc-license.txt
  */
-class Database_Expression
+class Database_Expression implements Countable
 {
 	// Raw expression string
 	protected $_value;
@@ -76,11 +76,19 @@ class Database_Expression
 	 */
 	public function compile(Database $db)
 	{
-		if (empty($this->_parameters))
+		if ($this->count() === 0)
 			return (string) $this->_value;
 
 		$position = 0;
 
 		return preg_replace('/(:\w++|\?)/e', '$db->quote($this->_parameters[ ("$1" === "?") ? $position++ : "$1" ])', $this->_value);
+	}
+
+	/**
+	 * Countable::count()
+	 */
+	public function count()
+	{
+		return count($this->_parameters);
 	}
 }
