45abd9e29c25252a74bfa4c00112e4fc7b1b3b00
Chris Bandy <chris.bandy@kohanaphp.com>
2010-01-17 10:32:04 -0600

    Delete Query tests

---
+++ tests/database/delete.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * @package RealDatabase
+ * @author  Chris Bandy
+ *
+ * @group   database
+ * @group   database.delete
+ */
+class Database_Delete_Test extends PHPUnit_Framework_TestCase
+{
+	public function test_from()
+	{
+		$db = new Database_Delete_Test_DB;
+		$query = new Database_Query_Delete;
+
+		$query->from('one', 'a');
+
+		$this->assertSame('DELETE FROM "pre_one" AS "a"', $db->quote($query));
+	}
+
+	public function test_using()
+	{
+		$db = new Database_Delete_Test_DB;
+		$query = new Database_Query_Delete('one');
+
+		$query->using('two', 'b');
+		$this->assertSame('DELETE FROM "pre_one" USING "pre_two" AS "b"', $db->quote($query));
+
+		$query->using('three');
+		$this->assertSame('DELETE FROM "pre_one" USING "pre_two" AS "b", "pre_three"', $db->quote($query));
+
+		$query->join('four');
+		$this->assertSame('DELETE FROM "pre_one" USING "pre_two" AS "b", "pre_three" JOIN "pre_four"', $db->quote($query));
+
+		$query->on(new Database_Query_Conditions(new Database_Column('four.x'), '=', 0));
+		$this->assertSame('DELETE FROM "pre_one" USING "pre_two" AS "b", "pre_three" JOIN "pre_four" ON ("pre_four"."x" = 0)', $db->quote($query));
+	}
+
+	public function test_where()
+	{
+		$db = new Database_Delete_Test_DB;
+		$query = new Database_Query_Delete('one');
+
+		$query->where(new Database_Query_Conditions(new Database_Column('one.x'), '=', 0));
+
+		$this->assertSame('DELETE FROM "pre_one" WHERE "pre_one"."x" = 0', $db->quote($query));
+	}
+}
+
+class Database_Delete_Test_DB extends Database
+{
+	public function escape($value)
+	{
+		return "'$value'";
+	}
+
+	public function table_prefix()
+	{
+		return 'pre_';
+	}
+}
