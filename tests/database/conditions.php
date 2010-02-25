cd603eeeacaa4314488094b7593a3e44d7ae48e1
Chris Bandy <chris.bandy@kohanaphp.com>
2010-02-25 09:32:31 -0600

    Conditions helpers tests

--- tests/database/conditions.php
+++ tests/database/conditions.php
@@ -16,6 +16,36 @@ class Database_Conditions_Test extends PHPUnit_Framework_TestCase
 		$this->assertSame("'2009-11-19' BETWEEN '2009-11-1' AND '2009-12-1'", $db->quote($conditions));
 	}
 
+	public function test_column()
+	{
+		$db = new Database_Conditions_Test_DB;
+		$conditions = new Database_Query_Conditions;
+
+		$this->assertSame($conditions, $conditions->column('and', 'a', '=', 0));
+		$this->assertSame('"a" = 0', $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->and_column('b', '<>', 'c'));
+		$this->assertSame('"a" = 0 AND "b" <> \'c\'', $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->or_column('d', 'IS', NULL));
+		$this->assertSame('"a" = 0 AND "b" <> \'c\' OR "d" IS NULL', $db->quote($conditions));
+	}
+
+	public function test_columns()
+	{
+		$db = new Database_Conditions_Test_DB;
+		$conditions = new Database_Query_Conditions;
+
+		$this->assertSame($conditions, $conditions->columns('and', 'a', '=', 'b'));
+		$this->assertSame('"a" = "b"', $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->and_columns('c', '<>', 'd'));
+		$this->assertSame('"a" = "b" AND "c" <> "d"', $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->or_columns('e', '=', 'f'));
+		$this->assertSame('"a" = "b" AND "c" <> "d" OR "e" = "f"', $db->quote($conditions));
+	}
+
 	public function test_in()
 	{
 		$db = new Database_Conditions_Test_DB;
@@ -64,6 +94,30 @@ class Database_Conditions_Test extends PHPUnit_Framework_TestCase
 
 		$this->assertSame($conditions, $conditions->close());
 		$this->assertSame('0 <> 1 AND (2 = 2 OR 2 = 2) OR (3 <> 4)', $db->quote($conditions));
+	}
+
+	public function test_parentheses_helpers()
+	{
+		$db = new Database_Conditions_Test_DB;
+		$conditions = new Database_Query_Conditions;
+
+		$this->assertSame($conditions, $conditions->and_open());
+		$this->assertSame('(', $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->or_open());
+		$this->assertSame('((', $db->quote($conditions));
+
+		$conditions->add('and', 0, '<>', 1);
+		$this->assertSame('((0 <> 1', $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->and_open());
+		$this->assertSame('((0 <> 1 AND (', $db->quote($conditions));
+
+		$conditions->add('and', 2, '=', 2);
+		$this->assertSame('((0 <> 1 AND (2 = 2', $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->or_open());
+		$this->assertSame('((0 <> 1 AND (2 = 2 OR (', $db->quote($conditions));
 	}
 }
 
