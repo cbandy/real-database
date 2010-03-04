86fb417388fa31b5615ba8bffec3fdb720e5e594
Chris Bandy <chris.bandy@kohanaphp.com>
2010-03-04 09:06:30 -0600

    DateTime object with persistent format

---
+++ classes/database/datetime.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * @package RealDatabase
+ *
+ * @author      Chris Bandy
+ * @copyright   (c) 2010 Chris Bandy
+ * @license     http://www.opensource.org/licenses/isc-license.txt
+ */
+class Database_DateTime extends DateTime
+{
+	/**
+	 * Format string for SQL-92 TIMESTAMP WITH TIME ZONE
+	 */
+	const SQL = 'Y-m-d H:i:sP';
+
+	/**
+	 * @var string  Format used to convert this object to string
+	 */
+	protected $_format;
+
+	/**
+	 * @param   string  $time       Time in a format accepted by strtotime()
+	 * @param   mixed   $timezone   Converted to DateTimeZone
+	 * @param   string  $format     Format accepted by date(), defaults to Database_DateTime::SQL
+	 */
+	public function __construct($time = 'now', $timezone = NULL, $format = Database_DateTime::SQL)
+	{
+		if ( ! $timezone)
+		{
+			// Passing NULL as the second argument raises an Exception
+			parent::__construct($time);
+		}
+		else
+		{
+			if ( ! $timezone instanceof DateTimeZone)
+			{
+				$timezone = new DateTimeZone($timezone);
+			}
+
+			parent::__construct($time, $timezone);
+		}
+
+		$this->_format = $format;
+	}
+
+	public function __toString()
+	{
+		return $this->format($this->_format);
+	}
+
+	/**
+	 * Resets the format used to convert this object to string
+	 *
+	 * @param   string  $format Format accepted by date()
+	 * @return  $this
+	 */
+	public function setFormat($format)
+	{
+		$this->_format = $format;
+
+		return $this;
+	}
+}
