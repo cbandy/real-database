e94919ce79a5a48853b7cedfa5c93137066b9b9a
Chris Bandy <chris.bandy@kohanaphp.com>
2010-03-06 10:34:16 -0600

    Countable unnecessary
    This commit reverts d4ee876d5c5058ed204c05111f9984fb798670e0

--- classes/database/expression.php
+++ classes/database/expression.php
@@ -7,7 +7,7 @@
  * @copyright   (c) 2010 Chris Bandy
  * @license     http://www.opensource.org/licenses/isc-license.txt
  */
-class Database_Expression implements Countable
+class Database_Expression
 {
 	/**
 	 * @var string  Raw expression
@@ -80,19 +80,11 @@ class Database_Expression implements Countable
 	 */
 	public function compile(Database $db)
 	{
-		if ($this->count() === 0)
+		if (empty($this->_parameters))
 			return (string) $this->_value;
 
 		$position = 0;
 
 		return preg_replace('/(:\w++|\?)/e', '$db->quote($this->_parameters[ ("$1" === "?") ? $position++ : "$1" ])', $this->_value);
-	}
-
-	/**
-	 * Countable::count()
-	 */
-	public function count()
-	{
-		return count($this->_parameters);
 	}
 }
