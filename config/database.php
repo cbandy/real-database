da1b74c14cabdbc9e921ac8031d7ada2d72b2f5e
Chris Bandy <chris.bandy@kohanaphp.com>
2010-04-04 11:04:30 -0500

    Default configuration

---
+++ config/database.php
@@ -0,0 +1,21 @@
+<?php
+
+return array
+(
+	'default' => array
+	(
+		'type'  => 'MySQL',
+		'charset'   => 'utf8',
+		'schema'    => NULL,
+		'connection' => array
+		(
+			'hostname' => 'localhost',
+			'port'     => NULL,
+			'username' => NULL,
+			'password' => NULL,
+			'database' => 'kohana',
+			'persistent' => FALSE,
+			'ssl'   => NULL,
+		),
+	),
+);
