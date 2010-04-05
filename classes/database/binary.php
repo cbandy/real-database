a3ffebe9bb8cad0a07a015ec9e64d15a0b7e0df4
Chris Bandy <chris.bandy@kohanaphp.com>
2010-04-05 10:05:41 -0500

    Wrapper class to distinguish between text and binary data

---
+++ classes/database/binary.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @package RealDatabase
+ *
+ * @author      Chris Bandy
+ * @copyright   (c) 2010 Chris Bandy
+ * @license     http://www.opensource.org/licenses/isc-license.txt
+ */
+class Database_Binary
+{
+	/**
+	 * @var mixed
+	 */
+	protected $_value;
+
+	/**
+	 * @param   mixed   $value
+	 */
+	public function __construct($value)
+	{
+		$this->_value = $value;
+	}
+
+	public function __toString()
+	{
+		return (string) $this->_value;
+	}
+}
