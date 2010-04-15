227651a1951bc52bc31433b42e3c682529018186
Chris Bandy <chris.bandy@kohanaphp.com>
2010-04-15 09:24:26 -0500

    Prepared Command and Query
    These default implementations quote parameters before executing raw SQL.

---
+++ classes/database/prepared/query.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @package RealDatabase
+ *
+ * @author      Chris Bandy
+ * @copyright   (c) 2010 Chris Bandy
+ * @license     http://www.opensource.org/licenses/isc-license.txt
+ */
+class Database_Prepared_Query extends Database_Prepared_Command
+{
+	protected $_as_object = FALSE;
+
+	/**
+	 * Return results as associative arrays when executed
+	 *
+	 * @return  $this
+	 */
+	public function as_assoc()
+	{
+		$this->_as_object = FALSE;
+
+		return $this;
+	}
+
+	/**
+	 * Return results as objects when executed
+	 *
+	 * @param   mixed   $class  Class to return or TRUE for stdClass
+	 * @return  $this
+	 */
+	public function as_object($class = TRUE)
+	{
+		$this->_as_object = $class;
+
+		return $this;
+	}
+
+	/**
+	 * Execute the query, returning the result set or NULL when the
+	 * statement is not a query (e.g., a DELETE statement)
+	 *
+	 * @throws  Database_Exception
+	 * @return  Database_Result Result set
+	 */
+	public function execute()
+	{
+		return $this->_db->execute_query($this->compile($this->_db), $this->_as_object);
+	}
+}
