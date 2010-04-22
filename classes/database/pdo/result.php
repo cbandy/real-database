7761aa92615d04f5cacd97d1f6692efebccd495f
Chris Bandy <chris.bandy@kohanaphp.com>
2010-04-22 07:50:15 -0500

    PDO: Extend array result and document the lack of PDO features

--- classes/database/pdo/result.php
+++ classes/database/pdo/result.php
@@ -6,23 +6,23 @@
  * @author      Chris Bandy
  * @copyright   (c) 2010 Chris Bandy
  * @license     http://www.opensource.org/licenses/isc-license.txt
+ *
+ * Prefetches all data since scrollable cursors do not work for most drivers
+ * and even PDOStatement->rowCount() should not be relied upon.
+ *
+ * @link http://bugs.php.net/44475  No MySQL cursor
+ * @link http://bugs.php.net/44861  No PostgreSQL cursor
+ * @link http://php.net/manual/pdostatement.rowcount
  */
-class Database_PDO_Result extends Database_Result
+class Database_PDO_Result extends Database_Result_Array
 {
-	/**
-	 * @var array   Fetched rows
-	 */
-	protected $_data;
-
 	/**
 	 * @param   PDOStatement    $statement
 	 * @param   mixed           $as_object
 	 */
 	public function __construct($statement, $as_object)
 	{
-		parent::__construct($statement, $as_object);
-
-		if ($as_object === FALSE)
+		if ( ! $as_object)
 		{
 			$statement->setFetchMode(PDO::FETCH_ASSOC);
 		}
@@ -35,23 +35,6 @@ class Database_PDO_Result extends Database_Result
 			$statement->setFetchMode(PDO::FETCH_OBJ);
 		}
 
-		$this->_data = $statement->fetchAll();
-
-		// PDOStatement->rowCount() should not be relied upon
-		// @link http://php.net/manual/pdostatement.rowcount
-		$this->_count = count($this->_data);
-	}
-
-	public function as_array($key = NULL, $value = NULL)
-	{
-		if ($key === NULL AND $value === NULL)
-			return $this->_data;
-
-		return parent::as_array($key, $value);
-	}
-
-	public function current()
-	{
-		return $this->_data[$this->_position];
+		parent::__construct($statement->fetchAll(), $as_object);
 	}
 }
