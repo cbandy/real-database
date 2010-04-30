42da273aec71522f9a73371111d49129540b3d01
Chris Bandy <chris.bandy@kohanaphp.com>
2010-04-30 07:35:15 -0500

    Numeric data type

---
+++ classes/database/numeric.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @package RealDatabase
+ *
+ * @author      Chris Bandy
+ * @copyright   (c) 2010 Chris Bandy
+ * @license     http://www.opensource.org/licenses/isc-license.txt
+ */
+class Database_Numeric
+{
+	/**
+	 * @var string  Format used to convert this object to string
+	 */
+	public $format;
+
+	/**
+	 * @var mixed
+	 */
+	public $value;
+
+	/**
+	 * @param   mixed   $value
+	 * @param   integer $scale  Number of digits in the fractional part
+	 */
+	public function __construct($value, $scale)
+	{
+		$this->format = '%.'.$scale.'F';
+		$this->value = $value;
+	}
+
+	/**
+	 * @uses sprintf()
+	 */
+	public function __toString()
+	{
+		return sprintf($this->format, $this->value);
+	}
+}
