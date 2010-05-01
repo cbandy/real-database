b016ff8a6b155c1a007f6595df1a2d735890c907
Chris Bandy <chris.bandy@kohanaphp.com>
2010-05-01 18:00:29 -0500

    Copyright and package for Exception

--- classes/database/exception.php
+++ classes/database/exception.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * @package RealDatabase
+ *
+ * @author      Chris Bandy
+ * @copyright   (c) 2010 Chris Bandy
+ * @license     http://www.opensource.org/licenses/isc-license.txt
+ */
 class Database_Exception extends Kohana_Exception
 {
 
