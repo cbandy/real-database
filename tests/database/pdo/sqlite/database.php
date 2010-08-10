bb00772b507221d3a2e331e92c0975078f03107b
Chris Bandy <chris.bandy@kohanaphp.com>
2010-08-10 16:42:07 -0500

    SQLite: Use unquoted table name but quote for raw SQL

--- tests/database/pdo/sqlite/database.php
+++ tests/database/pdo/sqlite/database.php
@@ -8,18 +8,17 @@
  */
 class Database_PDO_SQLite_Database_Test extends PHPUnit_Framework_TestCase
 {
-	protected $_table;
+	protected $_table = 'temp_test_table';
 
 	public function setUp()
 	{
 		$db = $this->sharedFixture;
+		$table = $db->quote_table($this->_table);
 
-		$this->_table = $db->quote_table('temp_test_table');
-
-		$db->execute_command('CREATE TEMPORARY TABLE '.$this->_table.' (id INTEGER PRIMARY KEY, value INTEGER)');
-		$db->execute_command('INSERT INTO '.$this->_table.' (value) VALUES (50)');
-		$db->execute_command('INSERT INTO '.$this->_table.' (value) VALUES (55)');
-		$db->execute_command('INSERT INTO '.$this->_table.' (value) VALUES (60)');
+		$db->execute_command('CREATE TEMPORARY TABLE '.$table.' (id INTEGER PRIMARY KEY, value INTEGER)');
+		$db->execute_command('INSERT INTO '.$table.' (value) VALUES (50)');
+		$db->execute_command('INSERT INTO '.$table.' (value) VALUES (55)');
+		$db->execute_command('INSERT INTO '.$table.' (value) VALUES (60)');
 	}
 
 	public function tearDown()
@@ -32,49 +31,55 @@ class Database_PDO_SQLite_Database_Test extends PHPUnit_Framework_TestCase
 	public function test_execute_command_query()
 	{
 		$db = $this->sharedFixture;
+		$table = $db->quote_table($this->_table);
 
-		$this->assertSame(1, $db->execute_command('SELECT * FROM '.$this->_table), 'Always one');
-		$this->assertSame(1, $db->execute_command('DELETE FROM '.$this->_table.' WHERE "id" = 1; SELECT * FROM '.$this->_table), 'Compound, always one');
+		$this->assertSame(1, $db->execute_command('SELECT * FROM '.$table), 'Always one');
+		$this->assertSame(1, $db->execute_command('DELETE FROM '.$table.' WHERE "id" = 1; SELECT * FROM '.$table), 'Compound, always one');
 	}
 
 	public function test_execute_compound_command()
 	{
 		$db = $this->sharedFixture;
+		$table = $db->quote_table($this->_table);
 
 		// All statements executed
-		$this->assertSame(2, $db->execute_command('DELETE FROM '.$this->_table.' WHERE "id" = 1; DELETE FROM '.$this->_table), 'Count of last statement');
+		$this->assertSame(2, $db->execute_command('DELETE FROM '.$table.' WHERE "id" = 1; DELETE FROM '.$table), 'Count of last statement');
 	}
 
 	public function test_execute_compound_command_mixed()
 	{
 		$db = $this->sharedFixture;
+		$table = $db->quote_table($this->_table);
 
-		$this->assertSame(3, $db->execute_command('SELECT * FROM '.$this->_table.' WHERE value < 60; DELETE FROM '.$this->_table), 'Count of last statement');
+		$this->assertSame(3, $db->execute_command('SELECT * FROM '.$table.' WHERE value < 60; DELETE FROM '.$table), 'Count of last statement');
 	}
 
 	public function test_execute_compound_query()
 	{
 		$db = $this->sharedFixture;
-		$result = $db->execute_query('SELECT * FROM '.$this->_table.' WHERE value < 60; SELECT * FROM '.$this->_table.' WHERE value < 70');
+		$table = $db->quote_table($this->_table);
+
+		$result = $db->execute_query('SELECT * FROM '.$table.' WHERE value < 60; SELECT * FROM '.$table.' WHERE value < 70');
 
 		$this->assertType('Database_Result', $result);
 		$this->assertSame(2, count($result), 'First result');
 		$this->assertEquals(array(50, 55), $result->as_array(NULL, 'value'), 'First result');
 
-		$this->assertType('Database_Result', $db->execute_query('SELECT * FROM '.$this->_table.' WHERE value < 60; DELETE FROM '.$this->_table));
-		$this->assertEquals(3, $db->execute_query('SELECT COUNT(*) FROM '.$this->_table)->get(), 'Second statement is not executed');
+		$this->assertType('Database_Result', $db->execute_query('SELECT * FROM '.$table.' WHERE value < 60; DELETE FROM '.$table));
+		$this->assertEquals(3, $db->execute_query('SELECT COUNT(*) FROM '.$table)->get(), 'Second statement is not executed');
 
-		$this->assertNull($db->execute_query('DELETE FROM '.$this->_table.' WHERE value = 50; DELETE FROM '.$this->_table.' WHERE value = 55; SELECT * FROM '.$this->_table));
-		$this->assertEquals(2, $db->execute_query('SELECT COUNT(*) FROM '.$this->_table)->get(), 'Only the first statement is executed');
+		$this->assertNull($db->execute_query('DELETE FROM '.$table.' WHERE value = 50; DELETE FROM '.$table.' WHERE value = 55; SELECT * FROM '.$table));
+		$this->assertEquals(2, $db->execute_query('SELECT COUNT(*) FROM '.$table)->get(), 'Only the first statement is executed');
 	}
 
 	public function test_execute_compound_query_mixed()
 	{
 		$db = $this->sharedFixture;
+		$table = $db->quote_table($this->_table);
 
-		$this->assertType('Database_Result', $db->execute_query('SELECT * FROM '.$this->_table.' WHERE value < 60; DELETE FROM '.$this->_table));
+		$this->assertType('Database_Result', $db->execute_query('SELECT * FROM '.$table.' WHERE value < 60; DELETE FROM '.$table));
 
-		$this->assertEquals(3, $db->execute_query('SELECT COUNT(*) FROM '.$this->_table)->get(), 'Second statement is not executed');
+		$this->assertEquals(3, $db->execute_query('SELECT COUNT(*) FROM '.$table)->get(), 'Second statement is not executed');
 	}
 
 	public function test_execute_insert()
@@ -82,13 +87,13 @@ class Database_PDO_SQLite_Database_Test extends PHPUnit_Framework_TestCase
 		$db = $this->sharedFixture;
 
 		$this->assertEquals(array(0,3), $db->execute_insert(''), 'Prior identity');
-		$this->assertEquals(array(1,4), $db->execute_insert('INSERT INTO '.$this->_table.' (value) VALUES (65)'));
+		$this->assertEquals(array(1,4), $db->execute_insert('INSERT INTO '.$db->quote_table($this->_table).' (value) VALUES (65)'));
 	}
 
 	public function test_insert()
 	{
 		$db = $this->sharedFixture;
-		$query = $db->insert('temp_test_table', array('value'));
+		$query = $db->insert($this->_table, array('value'));
 
 		$this->assertTrue($query instanceof Database_Command_Insert_Multiple);
 
@@ -164,6 +169,8 @@ class Database_PDO_SQLite_Database_Test extends PHPUnit_Framework_TestCase
 	public function test_table_columns($column, $expected)
 	{
 		$db = $this->sharedFixture;
+		$table = $db->quote_table($this->_table);
+
 		$expected = array_merge(array(
 			'column_name'       => 'field',
 			'ordinal_position'  => 1,
@@ -175,10 +182,10 @@ class Database_PDO_SQLite_Database_Test extends PHPUnit_Framework_TestCase
 			'numeric_scale'     => NULL,
 		), $expected);
 
-		$db->execute_command('DROP TABLE '.$this->_table);
-		$db->execute_command('CREATE TEMPORARY TABLE '.$this->_table."( field $column )");
+		$db->execute_command('DROP TABLE '.$table);
+		$db->execute_command('CREATE TEMPORARY TABLE '.$table."( field $column )");
 
-		$result = $db->table_columns('temp_test_table');
+		$result = $db->table_columns($this->_table);
 
 		$this->assertEquals($expected, $result['field']);
 	}
