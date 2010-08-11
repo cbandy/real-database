73c95ac3e2d28d341c18bf0b3ae680d32fdacd9c
Chris Bandy <chris.bandy@kohanaphp.com>
2010-08-11 00:04:18 -0500

    MySQL: Transaction tests

---
+++ tests/database/mysql/transaction.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once dirname(dirname(__FILE__)).'/abstract/transaction'.EXT;
+
+/**
+ * @package RealDatabase
+ * @author  Chris Bandy
+ *
+ * @group   database
+ * @group   database.mysql
+ */
+class Database_MySQL_Transaction_Test extends Database_Abstract_Transaction_Test
+{
+	protected $_table = 'temp_test_table';
+
+	public function setUp()
+	{
+		$db = $this->sharedFixture;
+
+		// Ensure the storage engine supports transactions
+		$db->execute_command('CREATE TEMPORARY TABLE '.$this->_table.' (value integer) ENGINE = InnoDB');
+		$db->execute_command('INSERT INTO '.$this->_table.' (value) VALUES (50)');
+	}
+
+	public function tearDown()
+	{
+		$db = $this->sharedFixture;
+
+		$db->disconnect();
+	}
+
+	public function test_command_provider()
+	{
+		return array
+		(
+			array('SELECT * FROM '.$this->_table, 'INSERT INTO '.$this->_table.' (value) VALUES (100)', array(array('value' => 50), array('value' => 100))),
+			array('SELECT * FROM '.$this->_table, 'DELETE FROM '.$this->_table.' WHERE VALUE = 50', array()),
+		);
+	}
+}
