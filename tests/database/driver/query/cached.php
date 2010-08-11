41faa26bd33c480d382cc55a88019a94dec1b92c
Chris Bandy <chris.bandy@kohanaphp.com>
2010-08-10 19:21:14 -0500

    Move cached query tests into driver suite

--- tests/database/driver/query/cached.php
+++ tests/database/driver/query/cached.php
@@ -4,34 +4,35 @@
  * @author  Chris Bandy
  *
  * @group   database
- * @group   database.query
- * @group   database.query.cached
+ * @group   database.queries
  */
-class Database_Query_Cached_Test extends PHPUnit_Framework_TestCase
+class Database_Driver_Query_Cached_Test extends PHPUnit_Framework_TestCase
 {
-	protected $_db;
-	protected $_table;
+	protected $_table = 'temp_test_table';
 
 	public function setUp()
 	{
-		$this->_db = Database::instance('testing');
-		$this->_table = $this->_db->quote_table('temp_test_table');
+		$db = $this->sharedFixture;
+		$table = $db->quote_table($this->_table);
 
-		$this->_db->execute_command('CREATE TEMPORARY TABLE '.$this->_table.' (value integer)');
-		$this->_db->execute_command('INSERT INTO '.$this->_table.' (value) VALUES (50)');
+		$db->execute_command('CREATE TEMPORARY TABLE '.$table.' (value integer)');
+		$db->execute_command('INSERT INTO '.$table.' (value) VALUES (50)');
 	}
 
 	public function tearDown()
 	{
-		$this->_db->disconnect();
+		$db = $this->sharedFixture;
+
+		$db->disconnect();
 	}
 
 	public function test_delete()
 	{
-		$query = new Database_Query('SELECT * FROM '.$this->_table);
-		$class = get_class($query->execute($this->_db));
+		$db = $this->sharedFixture;
+		$query = new Database_Query('SELECT * FROM '.$db->quote_table($this->_table));
+		$class = get_class($query->execute($db));
 
-		$cached = new Database_Query_Cached(5, $this->_db, $query);
+		$cached = new Database_Query_Cached(5, $db, $query);
 
 		// Cache the result
 		$cached->execute();
@@ -43,10 +44,11 @@ class Database_Query_Cached_Test extends PHPUnit_Framework_TestCase
 
 	public function test_execute()
 	{
-		$query = new Database_Query('SELECT * FROM '.$this->_table);
-		$class = get_class($query->execute($this->_db));
+		$db = $this->sharedFixture;
+		$query = new Database_Query('SELECT * FROM '.$db->quote_table($this->_table));
+		$class = get_class($query->execute($db));
 
-		$cached = new Database_Query_Cached(5, $this->_db, $query);
+		$cached = new Database_Query_Cached(5, $db, $query);
 
 		// Clear the cache
 		$cached->delete();
@@ -57,7 +59,8 @@ class Database_Query_Cached_Test extends PHPUnit_Framework_TestCase
 
 	public function test_as_assoc()
 	{
-		$cached = new Database_Query_Cached(5, $this->_db, new Database_Query('SELECT * FROM '.$this->_table));
+		$db = $this->sharedFixture;
+		$cached = new Database_Query_Cached(5, $db, new Database_Query('SELECT * FROM '.$db->quote_table($this->_table)));
 
 		// Clear the cache
 		$cached->delete();
@@ -69,7 +72,8 @@ class Database_Query_Cached_Test extends PHPUnit_Framework_TestCase
 
 	public function test_as_object()
 	{
-		$cached = new Database_Query_Cached(5, $this->_db, new Database_Query('SELECT * FROM '.$this->_table));
+		$db = $this->sharedFixture;
+		$cached = new Database_Query_Cached(5, $db, new Database_Query('SELECT * FROM '.$db->quote_table($this->_table)));
 
 		// Clear the cache
 		$cached->delete();
@@ -78,25 +82,35 @@ class Database_Query_Cached_Test extends PHPUnit_Framework_TestCase
 		$this->assertType('stdClass', $cached->execute()->current(), 'Object result');
 		$this->assertType('stdClass', $cached->execute()->current(), 'Object result, cached');
 
-		$this->assertSame($cached, $cached->as_object(), 'Chainable (TRUE)');
+		// Clear the cache
+		$cached->delete();
+
+		$this->assertSame($cached, $cached->as_object(TRUE), 'Chainable (TRUE)');
 		$this->assertType('stdClass', $cached->execute()->current(), 'Object result');
 		$this->assertType('stdClass', $cached->execute()->current(), 'Object result, cached');
+
+		// Clear the cache
+		$cached->delete();
 
 		$this->assertSame($cached, $cached->as_object(FALSE), 'Chainable (FALSE)');
 		$this->assertType('array', $cached->execute()->current(), 'Array result');
 		$this->assertType('array', $cached->execute()->current(), 'Array result, cached');
 
-		$this->assertSame($cached, $cached->as_object('Database_Query_Cached_Test_Class'), 'Chainable (Database_Query_Cached_Test_Class)');
-		$this->assertType('Database_Query_Cached_Test_Class', $cached->execute()->current(), 'Class result');
-		$this->assertType('Database_Query_Cached_Test_Class', $cached->execute()->current(), 'Class result, cached');
+		// Clear the cache
+		$cached->delete();
+
+		$this->assertSame($cached, $cached->as_object('Database_Driver_Query_Cached_Test_Class'), 'Chainable (Database_Driver_Query_Cached_Test_Class)');
+		$this->assertType('Database_Driver_Query_Cached_Test_Class', $cached->execute()->current(), 'Class result');
+		$this->assertType('Database_Driver_Query_Cached_Test_Class', $cached->execute()->current(), 'Class result, cached');
 	}
 
 	public function test_prepared()
 	{
-		$query = $this->_db->prepare_query('SELECT * FROM '.$this->_table);
+		$db = $this->sharedFixture;
+		$query = $db->prepare_query('SELECT * FROM '.$db->quote_table($this->_table));
 		$class = get_class($query->execute());
 
-		$cached = new Database_Query_Cached(5, $this->_db, $query);
+		$cached = new Database_Query_Cached(5, $db, $query);
 
 		// Clear the cache
 		$cached->delete();
@@ -106,4 +120,4 @@ class Database_Query_Cached_Test extends PHPUnit_Framework_TestCase
 	}
 }
 
-class Database_Query_Cached_Test_Class {}
+class Database_Driver_Query_Cached_Test_Class {}
