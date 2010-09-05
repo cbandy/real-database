c54cf4dc8213f93ecbb3743149960250218a56bf
Chris Bandy <chris.bandy@kohanaphp.com>
2010-09-05 11:18:23 -0500

    Test DDL constraints

---
+++ tests/database/base/ddl/constraint/check.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @package RealDatabase
+ * @author  Chris Bandy
+ *
+ * @group   database
+ * @group   database.ddl
+ */
+class Database_Base_DDL_Constraint_Check_Test extends PHPUnit_Framework_TestCase
+{
+	public function test_constructor()
+	{
+		$db = $this->sharedFixture;
+
+		$this->assertSame('CHECK (1)', $db->quote(new Database_DDL_Constraint_Check(new Database_Conditions(1))));
+	}
+
+	public function test_name()
+	{
+		$db = $this->sharedFixture;
+		$constraint = new Database_DDL_Constraint_Check(new Database_Conditions(1));
+
+		$this->assertSame($constraint, $constraint->name('a'));
+		$this->assertSame('CONSTRAINT "a" CHECK (1)', $db->quote($constraint));
+	}
+
+	public function test_conditions()
+	{
+		$db = $this->sharedFixture;
+		$constraint = new Database_DDL_Constraint_Check(new Database_Conditions(1));
+
+		$this->assertSame($constraint, $constraint->conditions(new Database_Conditions(2)));
+		$this->assertSame('CHECK (2)', $db->quote($constraint));
+	}
+}
---
+++ tests/database/base/ddl/constraint/unique.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @package RealDatabase
+ * @author  Chris Bandy
+ *
+ * @group   database
+ * @group   database.ddl
+ */
+class Database_Base_DDL_Constraint_Unique_Test extends PHPUnit_Framework_TestCase
+{
+	public function test_constructor()
+	{
+		$db = $this->sharedFixture;
+
+		$this->assertSame('UNIQUE', $db->quote(new Database_DDL_Constraint_Unique));
+		$this->assertSame('UNIQUE', $db->quote(new Database_DDL_Constraint_Unique(array())));
+		$this->assertSame('UNIQUE ("a")', $db->quote(new Database_DDL_Constraint_Unique(array('a'))));
+	}
+
+	public function test_name()
+	{
+		$db = $this->sharedFixture;
+		$constraint = new Database_DDL_Constraint_Unique;
+
+		$this->assertSame($constraint, $constraint->name('a'));
+		$this->assertSame('CONSTRAINT "a" UNIQUE', $db->quote($constraint));
+	}
+
+	public function test_columns()
+	{
+		$db = $this->sharedFixture;
+		$constraint = new Database_DDL_Constraint_Unique;
+
+		$this->assertSame($constraint, $constraint->columns(array('a')));
+		$this->assertSame('UNIQUE ("a")', $db->quote($constraint));
+	}
+}
