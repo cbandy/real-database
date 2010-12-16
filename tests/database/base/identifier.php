8f642add8d8990db346ce8c2873a353b14bd8e13
Chris Bandy <bandy.chris@gmail.com>
2010-12-15 22:51:48 -0600

    Identifier tests

---
+++ tests/database/base/identifier.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @package RealDatabase
+ * @author  Chris Bandy
+ *
+ * @group   database
+ * @group   database.identifiers
+ */
+class Database_Base_Identifier_Test extends PHPUnit_Framework_TestCase
+{
+	public function provider_constructor()
+	{
+		$a = new stdClass;
+		$b = new stdClass;
+		$c = new stdClass;
+
+		return array(
+			array('', '', array()),
+			array('a', 'a', array()),
+			array('b.c', 'c', array('b')),
+			array('d.e.f', 'f', array('d','e')),
+			array(array(), NULL, array()),
+			array(array('a'), 'a', array()),
+			array(array('b','c'), 'c', array('b')),
+			array(array('d','e','f'), 'f', array('d','e')),
+			array(array($a), $a, array()),
+			array(array($a, $b), $b, array($a)),
+			array(array($a, $b, $c), $c, array($a, $b)),
+		);
+	}
+
+	/**
+	 * @covers  Database_Identifier::__construct
+	 * @dataProvider    provider_constructor
+	 *
+	 * @param   array|string    $argument   Argument to the constructor
+	 * @param   mixed           $name       Expected name
+	 * @param   mixed           $namespace  Expected namespace
+	 */
+	public function test_constructor($argument, $name, $namespace)
+	{
+		$identifier = new Database_Identifier($argument);
+
+		$this->assertSame($name, $identifier->name);
+		$this->assertSame($namespace, $identifier->namespace);
+	}
+}
