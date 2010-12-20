650c3d077762eb98b6651b67d427e41cb5e61a9e
Chris Bandy <bandy.chris@gmail.com>
2010-12-20 13:35:17 -0600

    Refactor From tests. Add @covers directive

--- tests/database/base/from.php
+++ tests/database/base/from.php
@@ -8,15 +8,25 @@
  */
 class Database_Base_From_Test extends PHPUnit_Framework_TestCase
 {
-	public function test_add()
+	/**
+	 * @param   string  $method     Method to call
+	 * @param   string  $expected   Expected join type
+	 */
+	protected function _test_join_helper($method, $expected)
 	{
 		$db = $this->sharedFixture;
 		$from = new Database_From('one');
 
-		$this->assertSame($from, $from->add('two', 'b'));
-		$this->assertSame('"pre_one", "pre_two" AS "b"', $db->quote($from));
+		$this->assertSame($from, $from->$method('two'), 'Chainable (string)');
+		$this->assertSame('"pre_one" '.$expected.' JOIN "pre_two"', $db->quote($from));
+
+		$this->assertSame($from, $from->$method('three', 'a'), 'Chainable (string, string)');
+		$this->assertSame('"pre_one" '.$expected.' JOIN "pre_two" '.$expected.' JOIN "pre_three" AS "a"', $db->quote($from));
 	}
 
+	/**
+	 * @covers  Database_From::__construct
+	 */
 	public function test_constructor()
 	{
 		$db = $this->sharedFixture;
@@ -26,6 +36,22 @@ class Database_Base_From_Test extends PHPUnit_Framework_TestCase
 		$this->assertSame('"pre_one" AS "a"', $db->quote(new Database_From('one', 'a')));
 	}
 
+	/**
+	 * @covers  Database_From::_add
+	 * @covers  Database_From::add
+	 */
+	public function test_add()
+	{
+		$db = $this->sharedFixture;
+		$from = new Database_From('one');
+
+		$this->assertSame($from, $from->add('two', 'b'));
+		$this->assertSame('"pre_one", "pre_two" AS "b"', $db->quote($from));
+	}
+
+	/**
+	 * @covers  Database_From::join
+	 */
 	public function test_join()
 	{
 		$db = $this->sharedFixture;
@@ -39,37 +65,80 @@ class Database_Base_From_Test extends PHPUnit_Framework_TestCase
 	}
 
 	/**
-	 * @dataProvider provider_join_helpers
+	 * @covers  Database_From::cross_join
 	 */
-	public function test_join_helpers($method, $expected)
+	public function test_cross_join()
 	{
-		$db = $this->sharedFixture;
-		$from = new Database_From('one');
-
-		$this->assertSame($from, $from->$method('two'));
-		$this->assertSame('"pre_one" '.$expected.' JOIN "pre_two"', $db->quote($from));
-
-		$this->assertSame($from, $from->$method('three', 'a'));
-		$this->assertSame('"pre_one" '.$expected.' JOIN "pre_two" '.$expected.' JOIN "pre_three" AS "a"', $db->quote($from));
+		$this->_test_join_helper('cross_join', 'CROSS');
 	}
 
-	public function provider_join_helpers()
+	/**
+	 * @covers  Database_From::full_join
+	 */
+	public function test_full_join()
 	{
-		return array
-		(
-			array('cross_join', 'CROSS'),
-			array('full_join',  'FULL'),
-			array('inner_join', 'INNER'),
-			array('left_join',  'LEFT'),
-			array('right_join', 'RIGHT'),
-
-			array('natural_full_join',  'NATURAL FULL'),
-			array('natural_inner_join', 'NATURAL INNER'),
-			array('natural_left_join',  'NATURAL LEFT'),
-			array('natural_right_join', 'NATURAL RIGHT'),
-		);
+		$this->_test_join_helper('full_join', 'FULL');
 	}
 
+	/**
+	 * @covers  Database_From::inner_join
+	 */
+	public function test_inner_join()
+	{
+		$this->_test_join_helper('inner_join', 'INNER');
+	}
+
+	/**
+	 * @covers  Database_From::left_join
+	 */
+	public function test_left_join()
+	{
+		$this->_test_join_helper('left_join', 'LEFT');
+	}
+
+	/**
+	 * @covers  Database_From::right_join
+	 */
+	public function test_right_join()
+	{
+		$this->_test_join_helper('right_join', 'RIGHT');
+	}
+
+	/**
+	 * @covers  Database_From::natural_full_join
+	 */
+	public function test_natural_full_join()
+	{
+		$this->_test_join_helper('natural_full_join', 'NATURAL FULL');
+	}
+
+	/**
+	 * @covers  Database_From::natural_inner_join
+	 */
+	public function test_natural_inner_join()
+	{
+		$this->_test_join_helper('natural_inner_join', 'NATURAL INNER');
+	}
+
+	/**
+	 * @covers  Database_From::natural_left_join
+	 */
+	public function test_natural_left_join()
+	{
+		$this->_test_join_helper('natural_left_join', 'NATURAL LEFT');
+	}
+
+	/**
+	 * @covers  Database_From::natural_right_join
+	 */
+	public function test_natural_right_join()
+	{
+		$this->_test_join_helper('natural_right_join', 'NATURAL RIGHT');
+	}
+
+	/**
+	 * @covers  Database_From::on
+	 */
 	public function test_on()
 	{
 		$db = $this->sharedFixture;
@@ -88,6 +157,10 @@ class Database_Base_From_Test extends PHPUnit_Framework_TestCase
 		$this->assertSame('"pre_one" JOIN "pre_two" ON ("pre_one"."y" = "pre_two"."y")', $db->quote($from));
 	}
 
+	/**
+	 * @covers  Database_From::open
+	 * @covers  Database_From::close
+	 */
 	public function test_parentheses()
 	{
 		$db = $this->sharedFixture;
@@ -115,6 +188,9 @@ class Database_Base_From_Test extends PHPUnit_Framework_TestCase
 		$this->assertSame('("pre_one" AS "a", ("pre_two" JOIN "pre_three"))', $db->quote($from));
 	}
 
+	/**
+	 * @covers  Database_From::using
+	 */
 	public function test_using()
 	{
 		$db = $this->sharedFixture;
