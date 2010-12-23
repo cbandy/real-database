b02b12c8f4528dc32a660bc20423f967a99c8a2e
Chris Bandy <bandy.chris@gmail.com>
2010-12-23 09:42:35 -0600

    Refactor Query tests. Add @covers tags

--- tests/database/base/query.php
+++ tests/database/base/query.php
@@ -8,44 +8,80 @@
  */
 class Database_Base_Query_Test extends PHPUnit_Framework_TestCase
 {
+	/**
+	 * @covers  Database_Query::as_assoc
+	 */
 	public function test_as_assoc()
 	{
-		$db = $this->getMock('Database_Base_TestSuite_Database', array('execute_query'));
 		$query = new Database_Query('a');
 
-		$this->assertSame($query, $query->as_assoc());
-
-		$db->expects($this->once())
-			->method('execute_query')
-			->with($this->equalTo('a'), FALSE);
-
-		$query->execute($db);
-	}
-
-	/**
-	 * @dataProvider    provider_as_object
-	 */
-	public function test_as_object($sql, $as_object)
-	{
-		$db = $this->getMock('Database_Base_TestSuite_Database', array('execute_query'));
-		$query = new Database_Query($sql);
-
-		$this->assertSame($query, $query->as_object($as_object));
-
-		$db->expects($this->once())
-			->method('execute_query')
-			->with($this->equalTo($sql), $as_object);
-
-		$query->execute($db);
+		$this->assertSame($query, $query->as_assoc(), 'Chainable');
+		$this->assertSame(FALSE, $query->as_object);
 	}
 
 	public function provider_as_object()
 	{
 		return array
 		(
-			array('a', FALSE),
-			array('a', TRUE),
-			array('a', 'b'),
+			array(FALSE),
+			array(TRUE),
+			array('b'),
 		);
+	}
+
+	/**
+	 * @covers  Database_Query::as_object
+	 * @dataProvider    provider_as_object
+	 *
+	 * @param   string|boolean  $as_object  Expected value
+	 */
+	public function test_as_object($as_object)
+	{
+		$query = new Database_Query('a');
+
+		$this->assertSame($query, $query->as_object($as_object), 'Chainable');
+		$this->assertSame($as_object, $query->as_object);
+	}
+
+	/**
+	 * @covers  Database_Query::execute
+	 */
+	public function test_execute()
+	{
+		$db = $this->getMock('Database_Base_TestSuite_Database', array('execute_query'));
+		$db->expects($this->once())
+			->method('execute_query')
+			->with($this->equalTo('a'), FALSE);
+
+		$query = new Database_Query('a');
+		$query->execute($db);
+	}
+
+	public function provider_prepare()
+	{
+		return array
+		(
+			array('a', array()),
+			array('b', array('c')),
+			array('d ?', array('e')),
+		);
+	}
+
+	/**
+	 * @covers  Database_Query::prepare
+	 * @dataProvider    provider_prepare
+	 *
+	 * @param   string  $sql        Expected SQL
+	 * @param   array   $parameters Expected parameters
+	 */
+	public function test_prepare($sql, $parameters)
+	{
+		$db = $this->getMock('Database_Base_TestSuite_Database', array('prepare_query'));
+		$db->expects($this->once())
+			->method('prepare_query')
+			->with($this->equalTo($sql), $parameters);
+
+		$query = new Database_Query($sql, $parameters);
+		$query->prepare($db);
 	}
 }
