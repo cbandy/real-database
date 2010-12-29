c9753fd883f44510349121436c20381fe26c3261
Chris Bandy <bandy.chris@gmail.com>
2010-12-29 00:28:12 -0600

    Refactor DDL_Constraint_Primary tests. Add @covers tags

--- tests/database/base/ddl/constraint/primary.php
+++ tests/database/base/ddl/constraint/primary.php
@@ -8,6 +8,9 @@
  */
 class Database_Base_DDL_Constraint_Primary_Test extends PHPUnit_Framework_TestCase
 {
+	/**
+	 * @covers  Database_DDL_Constraint_Primary::__construct
+	 */
 	public function test_constructor()
 	{
 		$db = $this->sharedFixture;
@@ -17,12 +20,28 @@ class Database_Base_DDL_Constraint_Primary_Test extends PHPUnit_Framework_TestCa
 		$this->assertSame('PRIMARY KEY ("a")', $db->quote(new Database_DDL_Constraint_Primary(array('a'))));
 	}
 
+	/**
+	 * @covers  Database_DDL_Constraint_Primary::columns
+	 */
 	public function test_columns()
 	{
 		$db = $this->sharedFixture;
 		$constraint = new Database_DDL_Constraint_Primary;
 
-		$this->assertSame($constraint, $constraint->columns(array('a')));
+		$this->assertSame($constraint, $constraint->columns(array('a')), 'Chainable');
 		$this->assertSame('PRIMARY KEY ("a")', $db->quote($constraint));
+	}
+
+	/**
+	 * @covers  Database_DDL_Constraint_Primary::__toString
+	 */
+	public function test_toString()
+	{
+		$constraint = new Database_DDL_Constraint_Primary;
+		$constraint
+			->name('a')
+			->columns(array('b'));
+
+		$this->assertSame('CONSTRAINT :name PRIMARY KEY (:columns)', (string) $constraint);
 	}
 }
