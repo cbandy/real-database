02f15851ebbaa103a8347327bddd106ff5a81ddc
Chris Bandy <bandy.chris@gmail.com>
2010-12-29 00:32:13 -0600

    Refactor DDL_Constraint_Unique tests. Add @covers tags

--- tests/database/base/ddl/constraint/unique.php
+++ tests/database/base/ddl/constraint/unique.php
@@ -8,6 +8,9 @@
  */
 class Database_Base_DDL_Constraint_Unique_Test extends PHPUnit_Framework_TestCase
 {
+	/**
+	 * @covers  Database_DDL_Constraint_Unique::__construct
+	 */
 	public function test_constructor()
 	{
 		$db = $this->sharedFixture;
@@ -17,12 +20,28 @@ class Database_Base_DDL_Constraint_Unique_Test extends PHPUnit_Framework_TestCas
 		$this->assertSame('UNIQUE ("a")', $db->quote(new Database_DDL_Constraint_Unique(array('a'))));
 	}
 
+	/**
+	 * @covers  Database_DDL_Constraint_Unique::columns
+	 */
 	public function test_columns()
 	{
 		$db = $this->sharedFixture;
 		$constraint = new Database_DDL_Constraint_Unique;
 
-		$this->assertSame($constraint, $constraint->columns(array('a')));
+		$this->assertSame($constraint, $constraint->columns(array('a')), 'Chainable');
 		$this->assertSame('UNIQUE ("a")', $db->quote($constraint));
+	}
+
+	/**
+	 * @covers  Database_DDL_Constraint_Unique::__toString
+	 */
+	public function test_toString()
+	{
+		$constraint = new Database_DDL_Constraint_Unique;
+		$constraint
+			->name('a')
+			->columns(array('b'));
+
+		$this->assertSame('CONSTRAINT :name UNIQUE (:columns)', (string) $constraint);
 	}
 }
