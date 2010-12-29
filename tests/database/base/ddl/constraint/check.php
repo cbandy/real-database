d9e8a55becf4466a57022d364bf246df928c35b9
Chris Bandy <bandy.chris@gmail.com>
2010-12-29 00:23:30 -0600

    Refactor DDL_Constraint_Check tests. Add @covers tags

--- tests/database/base/ddl/constraint/check.php
+++ tests/database/base/ddl/constraint/check.php
@@ -8,19 +8,39 @@
  */
 class Database_Base_DDL_Constraint_Check_Test extends PHPUnit_Framework_TestCase
 {
+	/**
+	 * @covers  Database_DDL_Constraint_Check::__construct
+	 */
 	public function test_constructor()
 	{
 		$db = $this->sharedFixture;
 
+		$this->assertSame('CHECK (:conditions)', $db->quote(new Database_DDL_Constraint_Check));
 		$this->assertSame('CHECK (1)', $db->quote(new Database_DDL_Constraint_Check(new Database_Conditions(1))));
 	}
 
+	/**
+	 * @covers  Database_DDL_Constraint_Check::conditions
+	 */
 	public function test_conditions()
 	{
 		$db = $this->sharedFixture;
-		$constraint = new Database_DDL_Constraint_Check(new Database_Conditions(1));
+		$constraint = new Database_DDL_Constraint_Check;
 
-		$this->assertSame($constraint, $constraint->conditions(new Database_Conditions(2)));
-		$this->assertSame('CHECK (2)', $db->quote($constraint));
+		$this->assertSame($constraint, $constraint->conditions(new Database_Conditions(1)), 'Chainable');
+		$this->assertSame('CHECK (1)', $db->quote($constraint));
+	}
+
+	/**
+	 * @covers  Database_DDL_Constraint_Check::__toString
+	 */
+	public function test_toString()
+	{
+		$constraint = new Database_DDL_Constraint_Check;
+		$constraint
+			->name('a')
+			->conditions(new Database_Conditions(1));
+
+		$this->assertSame('CONSTRAINT :name CHECK (:conditions)', (string) $constraint);
 	}
 }
