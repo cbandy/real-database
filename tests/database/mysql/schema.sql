c63624c78c46b4007fa05a625b61334c848177ab
Chris Bandy <bandy.chris@gmail.com>
2011-02-27 04:48:53 -0600

    MySQL: Test schema

---
+++ tests/database/mysql/schema.sql
@@ -0,0 +1,14 @@
+
+/**
+ * Schema required for MySQL tests.
+ *
+ * Change this to include the table_prefix, if desired.
+ *
+ * @package      RealDatabase
+ * @subpackage   MySQL
+ */
+
+CREATE TABLE kohana_test_table (
+    id bigint unsigned AUTO_INCREMENT PRIMARY KEY,
+    value integer
+);
