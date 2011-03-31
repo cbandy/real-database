72d80e2e2da4d88284a88614fc4c6919a13505f3
Chris Bandy <bandy.chris@gmail.com>
2011-03-31 05:04:50 -0500

    Allow string codes in Database_Exception. refs #3358

--- classes/database/exception.php
+++ classes/database/exception.php
@@ -8,8 +8,35 @@
  * @author      Chris Bandy
  * @copyright   (c) 2010 Chris Bandy
  * @license     http://www.opensource.org/licenses/isc-license.txt
+ *
+ * @link http://bugs.php.net/39615 Exception code cannot be string
  */
 class Database_Exception extends Kohana_Exception
 {
+	/**
+	 * @var string  Error code
+	 */
+	protected $_code;
 
+	/**
+	 * Creates a translated exception.
+	 *
+	 * @param   string  $message    Error message
+	 * @param   array   $variables  Translation variables
+	 * @param   string  $code       Error code
+	 */
+	public function __construct($message, $variables = NULL, $code = NULL)
+	{
+		$this->_code = $code;
+
+		parent::__construct($message, $variables, (int) $code);
+	}
+
+	/**
+	 * @return  string  Error code
+	 */
+	public function code()
+	{
+		return $this->_code;
+	}
 }
