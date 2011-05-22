2247da23c4cdfc07ef2293358f3690c749f21662
Chris Bandy <bandy.chris@gmail.com>
2011-05-22 00:08:08 -0500

    Change DML_Insert::columns() to append+reset

--- classes/sql/dml/insert.php
+++ classes/sql/dml/insert.php
@@ -66,26 +66,30 @@ class SQL_DML_Insert extends SQL_Expression
 	}
 
 	/**
-	 * Set the list of columns to be populated with values
+	 * Append multiple columns to be populated with values.
 	 *
-	 * @param   array|NULL  $columns    Each element converted to SQL_Column
+	 * @param   array   $columns    List of columns converted to SQL_Column or NULL to reset
 	 * @return  $this
 	 */
 	public function columns($columns)
 	{
-		if ($columns !== NULL)
+		if ($columns === NULL)
 		{
-			foreach ($columns as & $column)
+			$this->parameters[':columns'] = array();
+		}
+		else
+		{
+			foreach ($columns as $column)
 			{
 				if ( ! $column instanceof SQL_Expression
 					AND ! $column instanceof SQL_Identifier)
 				{
 					$column = new SQL_Column($column);
 				}
+
+				$this->parameters[':columns'][] = $column;
 			}
 		}
-
-		$this->parameters[':columns'] = $columns;
 
 		return $this;
 	}
