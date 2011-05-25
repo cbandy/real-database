4c0dda150f881236eb211365649067d407d58a87
Chris Bandy <bandy.chris@gmail.com>
2011-05-25 18:14:37 -0500

    Split SQL_Conditions tests into two files

--- tests/database/sql/conditions.php
+++ tests/database/sql/conditions.php
@@ -8,20 +8,11 @@
  */
 class Database_SQL_Conditions_Test extends PHPUnit_Framework_TestCase
 {
-	/**
-	 * @covers  SQL_Conditions::__construct
-	 */
-	public function test_constructor_empty()
-	{
-		$db = new SQL;
-
-		$this->assertSame('', $db->quote(new SQL_Conditions));
-	}
-
 	public function provider_constuctor()
 	{
-		return array
-		(
+		return array(
+			array(array(), ''),
+
 			array(array('a'),           "'a'"),
 			array(array('b', 'c'),      "'b' C NULL"),
 			array(array('d', 'e', 'f'), "'d' E 'f'"),
@@ -34,7 +25,11 @@ class Database_SQL_Conditions_Test extends PHPUnit_Framework_TestCase
 
 	/**
 	 * @covers  SQL_Conditions::__construct
+	 *
 	 * @dataProvider    provider_constuctor
+	 *
+	 * @param   array   $arguments  Arguments
+	 * @param   string  $expected
 	 */
 	public function test_constructor($arguments, $expected)
 	{
@@ -71,7 +66,9 @@ class Database_SQL_Conditions_Test extends PHPUnit_Framework_TestCase
 	public function test_add_between()
 	{
 		$db = new SQL;
-		$conditions = new SQL_Conditions('2009-11-19', 'between', array('2009-11-1', '2009-12-1'));
+		$conditions = new SQL_Conditions(
+			'2009-11-19', 'between', array('2009-11-1', '2009-12-1')
+		);
 
 		$this->assertSame("'2009-11-19' BETWEEN '2009-11-1' AND '2009-12-1'", $db->quote($conditions));
 	}
@@ -82,180 +79,11 @@ class Database_SQL_Conditions_Test extends PHPUnit_Framework_TestCase
 	public function test_add_in()
 	{
 		$db = new SQL;
-		$conditions = new SQL_Conditions(new SQL_Identifier('a'), 'in', array('x', 5, new SQL_Identifier('z')));
+		$conditions = new SQL_Conditions(
+			new SQL_Identifier('a'), 'in', array('x', 5, new SQL_Identifier('z'))
+		);
 
 		$this->assertSame('"a" IN (\'x\', 5, "z")', $db->quote($conditions));
-	}
-
-	/**
-	 * @covers  SQL_Conditions::and_column
-	 */
-	public function test_and_column()
-	{
-		$db = new SQL;
-		$conditions = new SQL_Conditions;
-
-		$this->assertSame($conditions, $conditions->and_column('a', '=', 0));
-		$this->assertSame('"a" = 0', $db->quote($conditions));
-
-		$this->assertSame($conditions, $conditions->and_column('b', '<>', 1));
-		$this->assertSame('"a" = 0 AND "b" <> 1', $db->quote($conditions));
-	}
-
-	/**
-	 * @covers  SQL_Conditions::and_columns
-	 */
-	public function test_and_columns()
-	{
-		$db = new SQL;
-		$conditions = new SQL_Conditions;
-
-		$this->assertSame($conditions, $conditions->and_columns('a', '=', 'b'));
-		$this->assertSame('"a" = "b"', $db->quote($conditions));
-
-		$this->assertSame($conditions, $conditions->and_columns('c', '<>', 'd'));
-		$this->assertSame('"a" = "b" AND "c" <> "d"', $db->quote($conditions));
-	}
-
-	/**
-	 * @covers  SQL_Conditions::and_not
-	 */
-	public function test_and_not()
-	{
-		$db = new SQL;
-		$conditions = new SQL_Conditions;
-
-		$this->assertSame($conditions, $conditions->and_not('a', '=', 0));
-		$this->assertSame("NOT 'a' = 0", $db->quote($conditions));
-
-		$this->assertSame($conditions, $conditions->and_not('b', '<>', 1));
-		$this->assertSame("NOT 'a' = 0 AND NOT 'b' <> 1", $db->quote($conditions));
-	}
-
-	/**
-	 * @covers  SQL_Conditions::and_not_column
-	 */
-	public function test_and_not_column()
-	{
-		$db = new SQL;
-		$conditions = new SQL_Conditions;
-
-		$this->assertSame($conditions, $conditions->and_not_column('a', '=', 0));
-		$this->assertSame('NOT "a" = 0', $db->quote($conditions));
-
-		$this->assertSame($conditions, $conditions->and_not_column('b', '<>', 1));
-		$this->assertSame('NOT "a" = 0 AND NOT "b" <> 1', $db->quote($conditions));
-	}
-
-	/**
-	 * @covers  SQL_Conditions::and_not_columns
-	 */
-	public function test_and_not_columns()
-	{
-		$db = new SQL;
-		$conditions = new SQL_Conditions;
-
-		$this->assertSame($conditions, $conditions->and_not_columns('a', '=', 'b'));
-		$this->assertSame('NOT "a" = "b"', $db->quote($conditions));
-
-		$this->assertSame($conditions, $conditions->and_not_columns('c', '<>', 'd'));
-		$this->assertSame('NOT "a" = "b" AND NOT "c" <> "d"', $db->quote($conditions));
-	}
-
-	/**
-	 * @covers  SQL_Conditions::and_not_open
-	 */
-	public function test_and_not_open()
-	{
-		$db = new SQL;
-		$conditions = new SQL_Conditions;
-
-		$this->assertSame($conditions, $conditions->and_not_open());
-		$this->assertSame("NOT (", $db->quote($conditions));
-
-		$this->assertSame($conditions, $conditions->and_not_open('a', '=', 0));
-		$this->assertSame("NOT (NOT ('a' = 0", $db->quote($conditions));
-
-		$this->assertSame($conditions, $conditions->and_not_open('b', '<>', 1));
-		$this->assertSame("NOT (NOT ('a' = 0 AND NOT ('b' <> 1", $db->quote($conditions));
-	}
-
-	/**
-	 * @covers  SQL_Conditions::and_not_open_column
-	 */
-	public function test_and_not_open_column()
-	{
-		$db = new SQL;
-		$conditions = new SQL_Conditions;
-
-		$this->assertSame($conditions, $conditions->and_not_open_column('a', '=', 0));
-		$this->assertSame('NOT ("a" = 0', $db->quote($conditions));
-
-		$this->assertSame($conditions, $conditions->and_not_open_column('b', '<>', 1));
-		$this->assertSame('NOT ("a" = 0 AND NOT ("b" <> 1', $db->quote($conditions));
-	}
-
-	/**
-	 * @covers  SQL_Conditions::and_not_open_columns
-	 */
-	public function test_and_not_open_columns()
-	{
-		$db = new SQL;
-		$conditions = new SQL_Conditions;
-
-		$this->assertSame($conditions, $conditions->and_not_open_columns('a', '=', 'b'));
-		$this->assertSame('NOT ("a" = "b"', $db->quote($conditions));
-
-		$this->assertSame($conditions, $conditions->and_not_open_columns('c', '<>', 'd'));
-		$this->assertSame('NOT ("a" = "b" AND NOT ("c" <> "d"', $db->quote($conditions));
-	}
-
-	/**
-	 * @covers  SQL_Conditions::and_open
-	 */
-	public function test_and_open()
-	{
-		$db = new SQL;
-		$conditions = new SQL_Conditions;
-
-		$this->assertSame($conditions, $conditions->and_open());
-		$this->assertSame("(", $db->quote($conditions));
-
-		$this->assertSame($conditions, $conditions->and_open('a', '=', 0));
-		$this->assertSame("(('a' = 0", $db->quote($conditions));
-
-		$this->assertSame($conditions, $conditions->and_open('b', '<>', 1));
-		$this->assertSame("(('a' = 0 AND ('b' <> 1", $db->quote($conditions));
-	}
-
-	/**
-	 * @covers  SQL_Conditions::and_open_column
-	 */
-	public function test_and_open_column()
-	{
-		$db = new SQL;
-		$conditions = new SQL_Conditions;
-
-		$this->assertSame($conditions, $conditions->and_open_column('a', '=', 0));
-		$this->assertSame('("a" = 0', $db->quote($conditions));
-
-		$this->assertSame($conditions, $conditions->and_open_column('b', '<>', 1));
-		$this->assertSame('("a" = 0 AND ("b" <> 1', $db->quote($conditions));
-	}
-
-	/**
-	 * @covers  SQL_Conditions::and_open_columns
-	 */
-	public function test_and_open_columns()
-	{
-		$db = new SQL;
-		$conditions = new SQL_Conditions;
-
-		$this->assertSame($conditions, $conditions->and_open_columns('a', '=', 'b'));
-		$this->assertSame('("a" = "b"', $db->quote($conditions));
-
-		$this->assertSame($conditions, $conditions->and_open_columns('c', '<>', 'd'));
-		$this->assertSame('("a" = "b" AND ("c" <> "d"', $db->quote($conditions));
 	}
 
 	/**
@@ -475,176 +303,5 @@ class Database_SQL_Conditions_Test extends PHPUnit_Framework_TestCase
 
 		$this->assertSame($conditions, $conditions->open_columns('and', 'e', '>', 'f'));
 		$this->assertSame('("a" = "b" OR ("c" <> "d" AND ("e" > "f"', $db->quote($conditions));
-	}
-
-	/**
-	 * @covers  SQL_Conditions::or_column
-	 */
-	public function test_or_column()
-	{
-		$db = new SQL;
-		$conditions = new SQL_Conditions;
-
-		$this->assertSame($conditions, $conditions->or_column('a', '=', 0));
-		$this->assertSame('"a" = 0', $db->quote($conditions));
-
-		$this->assertSame($conditions, $conditions->or_column('b', '<>', 1));
-		$this->assertSame('"a" = 0 OR "b" <> 1', $db->quote($conditions));
-	}
-
-	/**
-	 * @covers  SQL_Conditions::or_columns
-	 */
-	public function test_or_columns()
-	{
-		$db = new SQL;
-		$conditions = new SQL_Conditions;
-
-		$this->assertSame($conditions, $conditions->or_columns('a', '=', 'b'));
-		$this->assertSame('"a" = "b"', $db->quote($conditions));
-
-		$this->assertSame($conditions, $conditions->or_columns('c', '<>', 'd'));
-		$this->assertSame('"a" = "b" OR "c" <> "d"', $db->quote($conditions));
-	}
-
-	/**
-	 * @covers  SQL_Conditions::or_not
-	 */
-	public function test_or_not()
-	{
-		$db = new SQL;
-		$conditions = new SQL_Conditions;
-
-		$this->assertSame($conditions, $conditions->or_not('a', '=', 0));
-		$this->assertSame("NOT 'a' = 0", $db->quote($conditions));
-
-		$this->assertSame($conditions, $conditions->or_not('b', '<>', 1));
-		$this->assertSame("NOT 'a' = 0 OR NOT 'b' <> 1", $db->quote($conditions));
-	}
-
-	/**
-	 * @covers  SQL_Conditions::or_not_column
-	 */
-	public function test_or_not_column()
-	{
-		$db = new SQL;
-		$conditions = new SQL_Conditions;
-
-		$this->assertSame($conditions, $conditions->or_not_column('a', '=', 0));
-		$this->assertSame('NOT "a" = 0', $db->quote($conditions));
-
-		$this->assertSame($conditions, $conditions->or_not_column('b', '<>', 1));
-		$this->assertSame('NOT "a" = 0 OR NOT "b" <> 1', $db->quote($conditions));
-	}
-
-	/**
-	 * @covers  SQL_Conditions::or_not_columns
-	 */
-	public function test_or_not_columns()
-	{
-		$db = new SQL;
-		$conditions = new SQL_Conditions;
-
-		$this->assertSame($conditions, $conditions->or_not_columns('a', '=', 'b'));
-		$this->assertSame('NOT "a" = "b"', $db->quote($conditions));
-
-		$this->assertSame($conditions, $conditions->or_not_columns('c', '<>', 'd'));
-		$this->assertSame('NOT "a" = "b" OR NOT "c" <> "d"', $db->quote($conditions));
-	}
-
-	/**
-	 * @covers  SQL_Conditions::or_not_open
-	 */
-	public function test_or_not_open()
-	{
-		$db = new SQL;
-		$conditions = new SQL_Conditions;
-
-		$this->assertSame($conditions, $conditions->or_not_open());
-		$this->assertSame("NOT (", $db->quote($conditions));
-
-		$this->assertSame($conditions, $conditions->or_not_open('a', '=', 0));
-		$this->assertSame("NOT (NOT ('a' = 0", $db->quote($conditions));
-
-		$this->assertSame($conditions, $conditions->or_not_open('b', '<>', 1));
-		$this->assertSame("NOT (NOT ('a' = 0 OR NOT ('b' <> 1", $db->quote($conditions));
-	}
-
-	/**
-	 * @covers  SQL_Conditions::or_not_open_column
-	 */
-	public function test_or_not_open_column()
-	{
-		$db = new SQL;
-		$conditions = new SQL_Conditions;
-
-		$this->assertSame($conditions, $conditions->or_not_open_column('a', '=', 0));
-		$this->assertSame('NOT ("a" = 0', $db->quote($conditions));
-
-		$this->assertSame($conditions, $conditions->or_not_open_column('b', '<>', 1));
-		$this->assertSame('NOT ("a" = 0 OR NOT ("b" <> 1', $db->quote($conditions));
-	}
-
-	/**
-	 * @covers  SQL_Conditions::or_not_open_columns
-	 */
-	public function test_or_not_open_columns()
-	{
-		$db = new SQL;
-		$conditions = new SQL_Conditions;
-
-		$this->assertSame($conditions, $conditions->or_not_open_columns('a', '=', 'b'));
-		$this->assertSame('NOT ("a" = "b"', $db->quote($conditions));
-
-		$this->assertSame($conditions, $conditions->or_not_open_columns('c', '<>', 'd'));
-		$this->assertSame('NOT ("a" = "b" OR NOT ("c" <> "d"', $db->quote($conditions));
-	}
-
-	/**
-	 * @covers  SQL_Conditions::or_open
-	 */
-	public function test_or_open()
-	{
-		$db = new SQL;
-		$conditions = new SQL_Conditions;
-
-		$this->assertSame($conditions, $conditions->or_open());
-		$this->assertSame("(", $db->quote($conditions));
-
-		$this->assertSame($conditions, $conditions->or_open('a', '=', 0));
-		$this->assertSame("(('a' = 0", $db->quote($conditions));
-
-		$this->assertSame($conditions, $conditions->or_open('b', '<>', 1));
-		$this->assertSame("(('a' = 0 OR ('b' <> 1", $db->quote($conditions));
-	}
-
-	/**
-	 * @covers  SQL_Conditions::or_open_column
-	 */
-	public function test_or_open_column()
-	{
-		$db = new SQL;
-		$conditions = new SQL_Conditions;
-
-		$this->assertSame($conditions, $conditions->or_open_column('a', '=', 0));
-		$this->assertSame('("a" = 0', $db->quote($conditions));
-
-		$this->assertSame($conditions, $conditions->or_open_column('b', '<>', 1));
-		$this->assertSame('("a" = 0 OR ("b" <> 1', $db->quote($conditions));
-	}
-
-	/**
-	 * @covers  SQL_Conditions::or_open_columns
-	 */
-	public function test_or_open_columns()
-	{
-		$db = new SQL;
-		$conditions = new SQL_Conditions;
-
-		$this->assertSame($conditions, $conditions->or_open_columns('a', '=', 'b'));
-		$this->assertSame('("a" = "b"', $db->quote($conditions));
-
-		$this->assertSame($conditions, $conditions->or_open_columns('c', '<>', 'd'));
-		$this->assertSame('("a" = "b" OR ("c" <> "d"', $db->quote($conditions));
 	}
 }
---
+++ tests/database/sql/conditions/helpers.php
@@ -0,0 +1,352 @@
+<?php
+/**
+ * @package RealDatabase
+ * @author  Chris Bandy
+ *
+ * @group   database
+ * @group   database.expressions
+ */
+class Database_SQL_Conditions_Helpers_Test extends PHPUnit_Framework_TestCase
+{
+	/**
+	 * @covers  SQL_Conditions::and_column
+	 */
+	public function test_and_column()
+	{
+		$db = new SQL;
+		$conditions = new SQL_Conditions;
+
+		$this->assertSame($conditions, $conditions->and_column('a', '=', 0));
+		$this->assertSame('"a" = 0', $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->and_column('b', '<>', 1));
+		$this->assertSame('"a" = 0 AND "b" <> 1', $db->quote($conditions));
+	}
+
+	/**
+	 * @covers  SQL_Conditions::and_columns
+	 */
+	public function test_and_columns()
+	{
+		$db = new SQL;
+		$conditions = new SQL_Conditions;
+
+		$this->assertSame($conditions, $conditions->and_columns('a', '=', 'b'));
+		$this->assertSame('"a" = "b"', $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->and_columns('c', '<>', 'd'));
+		$this->assertSame('"a" = "b" AND "c" <> "d"', $db->quote($conditions));
+	}
+
+	/**
+	 * @covers  SQL_Conditions::and_not
+	 */
+	public function test_and_not()
+	{
+		$db = new SQL;
+		$conditions = new SQL_Conditions;
+
+		$this->assertSame($conditions, $conditions->and_not('a', '=', 0));
+		$this->assertSame("NOT 'a' = 0", $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->and_not('b', '<>', 1));
+		$this->assertSame("NOT 'a' = 0 AND NOT 'b' <> 1", $db->quote($conditions));
+	}
+
+	/**
+	 * @covers  SQL_Conditions::and_not_column
+	 */
+	public function test_and_not_column()
+	{
+		$db = new SQL;
+		$conditions = new SQL_Conditions;
+
+		$this->assertSame($conditions, $conditions->and_not_column('a', '=', 0));
+		$this->assertSame('NOT "a" = 0', $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->and_not_column('b', '<>', 1));
+		$this->assertSame('NOT "a" = 0 AND NOT "b" <> 1', $db->quote($conditions));
+	}
+
+	/**
+	 * @covers  SQL_Conditions::and_not_columns
+	 */
+	public function test_and_not_columns()
+	{
+		$db = new SQL;
+		$conditions = new SQL_Conditions;
+
+		$this->assertSame($conditions, $conditions->and_not_columns('a', '=', 'b'));
+		$this->assertSame('NOT "a" = "b"', $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->and_not_columns('c', '<>', 'd'));
+		$this->assertSame('NOT "a" = "b" AND NOT "c" <> "d"', $db->quote($conditions));
+	}
+
+	/**
+	 * @covers  SQL_Conditions::and_not_open
+	 */
+	public function test_and_not_open()
+	{
+		$db = new SQL;
+		$conditions = new SQL_Conditions;
+
+		$this->assertSame($conditions, $conditions->and_not_open());
+		$this->assertSame("NOT (", $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->and_not_open('a', '=', 0));
+		$this->assertSame("NOT (NOT ('a' = 0", $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->and_not_open('b', '<>', 1));
+		$this->assertSame("NOT (NOT ('a' = 0 AND NOT ('b' <> 1", $db->quote($conditions));
+	}
+
+	/**
+	 * @covers  SQL_Conditions::and_not_open_column
+	 */
+	public function test_and_not_open_column()
+	{
+		$db = new SQL;
+		$conditions = new SQL_Conditions;
+
+		$this->assertSame($conditions, $conditions->and_not_open_column('a', '=', 0));
+		$this->assertSame('NOT ("a" = 0', $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->and_not_open_column('b', '<>', 1));
+		$this->assertSame('NOT ("a" = 0 AND NOT ("b" <> 1', $db->quote($conditions));
+	}
+
+	/**
+	 * @covers  SQL_Conditions::and_not_open_columns
+	 */
+	public function test_and_not_open_columns()
+	{
+		$db = new SQL;
+		$conditions = new SQL_Conditions;
+
+		$this->assertSame($conditions, $conditions->and_not_open_columns('a', '=', 'b'));
+		$this->assertSame('NOT ("a" = "b"', $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->and_not_open_columns('c', '<>', 'd'));
+		$this->assertSame('NOT ("a" = "b" AND NOT ("c" <> "d"', $db->quote($conditions));
+	}
+
+	/**
+	 * @covers  SQL_Conditions::and_open
+	 */
+	public function test_and_open()
+	{
+		$db = new SQL;
+		$conditions = new SQL_Conditions;
+
+		$this->assertSame($conditions, $conditions->and_open());
+		$this->assertSame("(", $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->and_open('a', '=', 0));
+		$this->assertSame("(('a' = 0", $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->and_open('b', '<>', 1));
+		$this->assertSame("(('a' = 0 AND ('b' <> 1", $db->quote($conditions));
+	}
+
+	/**
+	 * @covers  SQL_Conditions::and_open_column
+	 */
+	public function test_and_open_column()
+	{
+		$db = new SQL;
+		$conditions = new SQL_Conditions;
+
+		$this->assertSame($conditions, $conditions->and_open_column('a', '=', 0));
+		$this->assertSame('("a" = 0', $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->and_open_column('b', '<>', 1));
+		$this->assertSame('("a" = 0 AND ("b" <> 1', $db->quote($conditions));
+	}
+
+	/**
+	 * @covers  SQL_Conditions::and_open_columns
+	 */
+	public function test_and_open_columns()
+	{
+		$db = new SQL;
+		$conditions = new SQL_Conditions;
+
+		$this->assertSame($conditions, $conditions->and_open_columns('a', '=', 'b'));
+		$this->assertSame('("a" = "b"', $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->and_open_columns('c', '<>', 'd'));
+		$this->assertSame('("a" = "b" AND ("c" <> "d"', $db->quote($conditions));
+	}
+
+	/**
+	 * @covers  SQL_Conditions::or_column
+	 */
+	public function test_or_column()
+	{
+		$db = new SQL;
+		$conditions = new SQL_Conditions;
+
+		$this->assertSame($conditions, $conditions->or_column('a', '=', 0));
+		$this->assertSame('"a" = 0', $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->or_column('b', '<>', 1));
+		$this->assertSame('"a" = 0 OR "b" <> 1', $db->quote($conditions));
+	}
+
+	/**
+	 * @covers  SQL_Conditions::or_columns
+	 */
+	public function test_or_columns()
+	{
+		$db = new SQL;
+		$conditions = new SQL_Conditions;
+
+		$this->assertSame($conditions, $conditions->or_columns('a', '=', 'b'));
+		$this->assertSame('"a" = "b"', $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->or_columns('c', '<>', 'd'));
+		$this->assertSame('"a" = "b" OR "c" <> "d"', $db->quote($conditions));
+	}
+
+	/**
+	 * @covers  SQL_Conditions::or_not
+	 */
+	public function test_or_not()
+	{
+		$db = new SQL;
+		$conditions = new SQL_Conditions;
+
+		$this->assertSame($conditions, $conditions->or_not('a', '=', 0));
+		$this->assertSame("NOT 'a' = 0", $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->or_not('b', '<>', 1));
+		$this->assertSame("NOT 'a' = 0 OR NOT 'b' <> 1", $db->quote($conditions));
+	}
+
+	/**
+	 * @covers  SQL_Conditions::or_not_column
+	 */
+	public function test_or_not_column()
+	{
+		$db = new SQL;
+		$conditions = new SQL_Conditions;
+
+		$this->assertSame($conditions, $conditions->or_not_column('a', '=', 0));
+		$this->assertSame('NOT "a" = 0', $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->or_not_column('b', '<>', 1));
+		$this->assertSame('NOT "a" = 0 OR NOT "b" <> 1', $db->quote($conditions));
+	}
+
+	/**
+	 * @covers  SQL_Conditions::or_not_columns
+	 */
+	public function test_or_not_columns()
+	{
+		$db = new SQL;
+		$conditions = new SQL_Conditions;
+
+		$this->assertSame($conditions, $conditions->or_not_columns('a', '=', 'b'));
+		$this->assertSame('NOT "a" = "b"', $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->or_not_columns('c', '<>', 'd'));
+		$this->assertSame('NOT "a" = "b" OR NOT "c" <> "d"', $db->quote($conditions));
+	}
+
+	/**
+	 * @covers  SQL_Conditions::or_not_open
+	 */
+	public function test_or_not_open()
+	{
+		$db = new SQL;
+		$conditions = new SQL_Conditions;
+
+		$this->assertSame($conditions, $conditions->or_not_open());
+		$this->assertSame("NOT (", $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->or_not_open('a', '=', 0));
+		$this->assertSame("NOT (NOT ('a' = 0", $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->or_not_open('b', '<>', 1));
+		$this->assertSame("NOT (NOT ('a' = 0 OR NOT ('b' <> 1", $db->quote($conditions));
+	}
+
+	/**
+	 * @covers  SQL_Conditions::or_not_open_column
+	 */
+	public function test_or_not_open_column()
+	{
+		$db = new SQL;
+		$conditions = new SQL_Conditions;
+
+		$this->assertSame($conditions, $conditions->or_not_open_column('a', '=', 0));
+		$this->assertSame('NOT ("a" = 0', $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->or_not_open_column('b', '<>', 1));
+		$this->assertSame('NOT ("a" = 0 OR NOT ("b" <> 1', $db->quote($conditions));
+	}
+
+	/**
+	 * @covers  SQL_Conditions::or_not_open_columns
+	 */
+	public function test_or_not_open_columns()
+	{
+		$db = new SQL;
+		$conditions = new SQL_Conditions;
+
+		$this->assertSame($conditions, $conditions->or_not_open_columns('a', '=', 'b'));
+		$this->assertSame('NOT ("a" = "b"', $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->or_not_open_columns('c', '<>', 'd'));
+		$this->assertSame('NOT ("a" = "b" OR NOT ("c" <> "d"', $db->quote($conditions));
+	}
+
+	/**
+	 * @covers  SQL_Conditions::or_open
+	 */
+	public function test_or_open()
+	{
+		$db = new SQL;
+		$conditions = new SQL_Conditions;
+
+		$this->assertSame($conditions, $conditions->or_open());
+		$this->assertSame("(", $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->or_open('a', '=', 0));
+		$this->assertSame("(('a' = 0", $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->or_open('b', '<>', 1));
+		$this->assertSame("(('a' = 0 OR ('b' <> 1", $db->quote($conditions));
+	}
+
+	/**
+	 * @covers  SQL_Conditions::or_open_column
+	 */
+	public function test_or_open_column()
+	{
+		$db = new SQL;
+		$conditions = new SQL_Conditions;
+
+		$this->assertSame($conditions, $conditions->or_open_column('a', '=', 0));
+		$this->assertSame('("a" = 0', $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->or_open_column('b', '<>', 1));
+		$this->assertSame('("a" = 0 OR ("b" <> 1', $db->quote($conditions));
+	}
+
+	/**
+	 * @covers  SQL_Conditions::or_open_columns
+	 */
+	public function test_or_open_columns()
+	{
+		$db = new SQL;
+		$conditions = new SQL_Conditions;
+
+		$this->assertSame($conditions, $conditions->or_open_columns('a', '=', 'b'));
+		$this->assertSame('("a" = "b"', $db->quote($conditions));
+
+		$this->assertSame($conditions, $conditions->or_open_columns('c', '<>', 'd'));
+		$this->assertSame('("a" = "b" OR ("c" <> "d"', $db->quote($conditions));
+	}
+}
