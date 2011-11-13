cc5693e5d70d266fa3cba583f47f63fe7ba9f5c3
Chris Bandy <bandy.chris@gmail.com>
2011-11-12 21:57:52 -0600

    Test SQL_DML_Set directly
    Follow-up to 61633a981ac950742a4bcaade0de52754831fdfe

--- tests/database/sql/dml/set.php
+++ tests/database/sql/dml/set.php
@@ -9,23 +9,23 @@
 class Database_SQL_DML_Set_Test extends PHPUnit_Framework_TestCase
 {
 	/**
-	 * @covers  Database_Query_Set::__construct
+	 * @covers  SQL_DML_Set::__construct
 	 */
 	public function test_constructor()
 	{
 		$db = new SQL;
 
-		$this->assertSame('', $db->quote(new Database_Query_Set));
-		$this->assertSame('(a)', $db->quote(new Database_Query_Set(new Database_Query('a'))));
+		$this->assertSame('', $db->quote(new SQL_DML_Set));
+		$this->assertSame('(a)', $db->quote(new SQL_DML_Set(new Database_Query('a'))));
 	}
 
 	/**
-	 * @covers  Database_Query_Set::add
+	 * @covers  SQL_DML_Set::add
 	 */
 	public function test_add()
 	{
 		$db = new SQL;
-		$query = new Database_Query_Set;
+		$query = new SQL_DML_Set;
 
 		$this->assertSame($query, $query->add('a', new Database_Query('b')));
 		$this->assertSame('(b)', $db->quote($query));
@@ -35,12 +35,12 @@ class Database_SQL_DML_Set_Test extends PHPUnit_Framework_TestCase
 	}
 
 	/**
-	 * @covers  Database_Query_Set::open
+	 * @covers  SQL_DML_Set::open
 	 */
 	public function test_open()
 	{
 		$db = new SQL;
-		$query = new Database_Query_Set;
+		$query = new SQL_DML_Set;
 
 		$this->assertSame($query, $query->open('a'), 'Chainable (void)');
 		$this->assertSame('(', $db->quote($query));
@@ -53,12 +53,12 @@ class Database_SQL_DML_Set_Test extends PHPUnit_Framework_TestCase
 	}
 
 	/**
-	 * @covers  Database_Query_Set::close
+	 * @covers  SQL_DML_Set::close
 	 */
 	public function test_close()
 	{
 		$db = new SQL;
-		$query = new Database_Query_Set;
+		$query = new SQL_DML_Set;
 
 		$this->assertSame($query, $query->close(), 'Chainable (1)');
 		$this->assertSame(')', $db->quote($query));
@@ -68,25 +68,25 @@ class Database_SQL_DML_Set_Test extends PHPUnit_Framework_TestCase
 	}
 
 	/**
-	 * @covers  Database_Query_Set::close
-	 * @covers  Database_Query_Set::open
+	 * @covers  SQL_DML_Set::close
+	 * @covers  SQL_DML_Set::open
 	 */
 	public function test_close_open()
 	{
 		$db = new SQL;
-		$query = new Database_Query_Set;
+		$query = new SQL_DML_Set;
 
 		$this->assertSame(') A (',      $db->quote($query->close()->open('a')));
 		$this->assertSame(') A () B (', $db->quote($query->close()->open('b')));
 	}
 
 	/**
-	 * @covers  Database_Query_Set::except
+	 * @covers  SQL_DML_Set::except
 	 */
 	public function test_except()
 	{
 		$db = new SQL;
-		$query = new Database_Query_Set(new Database_Query('a'));
+		$query = new SQL_DML_Set(new Database_Query('a'));
 
 		$this->assertSame($query, $query->except(new Database_Query('b')), 'Chainable (query)');
 		$this->assertSame('(a) EXCEPT (b)', $db->quote($query));
@@ -99,12 +99,12 @@ class Database_SQL_DML_Set_Test extends PHPUnit_Framework_TestCase
 	}
 
 	/**
-	 * @covers  Database_Query_Set::except_open
+	 * @covers  SQL_DML_Set::except_open
 	 */
 	public function test_except_open()
 	{
 		$db = new SQL;
-		$query = new Database_Query_Set(new Database_Query('a'));
+		$query = new SQL_DML_Set(new Database_Query('a'));
 
 		$this->assertSame($query, $query->except_open(), 'Chainable (void)');
 		$this->assertSame('(a) EXCEPT (', $db->quote($query));
@@ -124,7 +124,7 @@ class Database_SQL_DML_Set_Test extends PHPUnit_Framework_TestCase
 		$this->assertSame($query, $query->except_open(NULL, TRUE), 'Chainable (NULL, TRUE)');
 		$this->assertSame('(a) EXCEPT () EXCEPT () EXCEPT () EXCEPT ALL (', $db->quote($query));
 
-		$query = new Database_Query_Set(new Database_Query('a'));
+		$query = new SQL_DML_Set(new Database_Query('a'));
 
 		$this->assertSame($query, $query->except_open(new Database_Query('b')), 'Chainable (query)');
 		$this->assertSame('(a) EXCEPT ((b)', $db->quote($query));
@@ -137,12 +137,12 @@ class Database_SQL_DML_Set_Test extends PHPUnit_Framework_TestCase
 	}
 
 	/**
-	 * @covers  Database_Query_Set::intersect
+	 * @covers  SQL_DML_Set::intersect
 	 */
 	public function test_intersect()
 	{
 		$db = new SQL;
-		$query = new Database_Query_Set(new Database_Query('a'));
+		$query = new SQL_DML_Set(new Database_Query('a'));
 
 		$this->assertSame($query, $query->intersect(new Database_Query('b')), 'Chainable (query)');
 		$this->assertSame('(a) INTERSECT (b)', $db->quote($query));
@@ -155,12 +155,12 @@ class Database_SQL_DML_Set_Test extends PHPUnit_Framework_TestCase
 	}
 
 	/**
-	 * @covers  Database_Query_Set::intersect_open
+	 * @covers  SQL_DML_Set::intersect_open
 	 */
 	public function test_intersect_open()
 	{
 		$db = new SQL;
-		$query = new Database_Query_Set(new Database_Query('a'));
+		$query = new SQL_DML_Set(new Database_Query('a'));
 
 		$this->assertSame($query, $query->intersect_open(), 'Chainable (void)');
 		$this->assertSame('(a) INTERSECT (', $db->quote($query));
@@ -180,7 +180,7 @@ class Database_SQL_DML_Set_Test extends PHPUnit_Framework_TestCase
 		$this->assertSame($query, $query->intersect_open(NULL, TRUE), 'Chainable (NULL, TRUE)');
 		$this->assertSame('(a) INTERSECT () INTERSECT () INTERSECT () INTERSECT ALL (', $db->quote($query));
 
-		$query = new Database_Query_Set(new Database_Query('a'));
+		$query = new SQL_DML_Set(new Database_Query('a'));
 
 		$this->assertSame($query, $query->intersect_open(new Database_Query('b')), 'Chainable (query)');
 		$this->assertSame('(a) INTERSECT ((b)', $db->quote($query));
@@ -193,12 +193,12 @@ class Database_SQL_DML_Set_Test extends PHPUnit_Framework_TestCase
 	}
 
 	/**
-	 * @covers  Database_Query_Set::union
+	 * @covers  SQL_DML_Set::union
 	 */
 	public function test_union()
 	{
 		$db = new SQL;
-		$query = new Database_Query_Set(new Database_Query('a'));
+		$query = new SQL_DML_Set(new Database_Query('a'));
 
 		$this->assertSame($query, $query->union(new Database_Query('b')), 'Chainable (query)');
 		$this->assertSame('(a) UNION (b)', $db->quote($query));
@@ -211,12 +211,12 @@ class Database_SQL_DML_Set_Test extends PHPUnit_Framework_TestCase
 	}
 
 	/**
-	 * @covers  Database_Query_Set::union_open
+	 * @covers  SQL_DML_Set::union_open
 	 */
 	public function test_union_open()
 	{
 		$db = new SQL;
-		$query = new Database_Query_Set(new Database_Query('a'));
+		$query = new SQL_DML_Set(new Database_Query('a'));
 
 		$this->assertSame($query, $query->union_open(), 'Chainable (void)');
 		$this->assertSame('(a) UNION (', $db->quote($query));
@@ -236,7 +236,7 @@ class Database_SQL_DML_Set_Test extends PHPUnit_Framework_TestCase
 		$this->assertSame($query, $query->union_open(NULL, TRUE), 'Chainable (NULL, TRUE)');
 		$this->assertSame('(a) UNION () UNION () UNION () UNION ALL (', $db->quote($query));
 
-		$query = new Database_Query_Set(new Database_Query('a'));
+		$query = new SQL_DML_Set(new Database_Query('a'));
 
 		$this->assertSame($query, $query->union_open(new Database_Query('b')), 'Chainable (query)');
 		$this->assertSame('(a) UNION ((b)', $db->quote($query));
@@ -249,13 +249,13 @@ class Database_SQL_DML_Set_Test extends PHPUnit_Framework_TestCase
 	}
 
 	/**
-	 * @covers  Database_Query_Set::__toString
-	 * @covers  Database_Query_Set::order_by
+	 * @covers  SQL_DML_Set::__toString
+	 * @covers  SQL_DML_Set::order_by
 	 */
 	public function test_order_by()
 	{
 		$db = new SQL;
-		$query = new Database_Query_Set(new Database_Query('a'));
+		$query = new SQL_DML_Set(new Database_Query('a'));
 
 		$this->assertSame($query, $query->order_by('x'), 'Chainable (string)');
 		$this->assertSame('(a) ORDER BY "x"', $db->quote($query));
@@ -271,13 +271,13 @@ class Database_SQL_DML_Set_Test extends PHPUnit_Framework_TestCase
 	}
 
 	/**
-	 * @covers  Database_Query_Set::__toString
-	 * @covers  Database_Query_Set::limit
+	 * @covers  SQL_DML_Set::__toString
+	 * @covers  SQL_DML_Set::limit
 	 */
 	public function test_limit()
 	{
 		$db = new SQL;
-		$query = new Database_Query_Set(new Database_Query('a'));
+		$query = new SQL_DML_Set(new Database_Query('a'));
 
 		$this->assertSame($query, $query->limit(5));
 		$this->assertSame('(a) LIMIT 5', $db->quote($query));
@@ -287,13 +287,13 @@ class Database_SQL_DML_Set_Test extends PHPUnit_Framework_TestCase
 	}
 
 	/**
-	 * @covers  Database_Query_Set::__toString
-	 * @covers  Database_Query_Set::offset
+	 * @covers  SQL_DML_Set::__toString
+	 * @covers  SQL_DML_Set::offset
 	 */
 	public function test_offset()
 	{
 		$db = new SQL;
-		$query = new Database_Query_Set(new Database_Query('a'));
+		$query = new SQL_DML_Set(new Database_Query('a'));
 
 		$this->assertSame($query, $query->offset(5));
 		$this->assertSame('(a) OFFSET 5', $db->quote($query));
